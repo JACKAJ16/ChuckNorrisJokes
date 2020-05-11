47bdb69fb3a0cbaaa2526c04102e3d5883fda8eb
JACKAJ16 <zhenya271@gmail.com>
2020-05-12 00:39:33 +0300

    uploading build

---
+++ precache-manifest.b9aac3dfb1c13430edb45244366ea3aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70963b6df30e3b4365725d83a41d51aa",
+    "url": "/ChuckNorrisJokes/index.html"
+  },
+  {
+    "revision": "4c2b341812e042f3fcf9",
+    "url": "/ChuckNorrisJokes/static/css/main.ac8eb0b0.chunk.css"
+  },
+  {
+    "revision": "2806e10b58d1d1296f6b",
+    "url": "/ChuckNorrisJokes/static/js/2.26f7cb7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChuckNorrisJokes/static/js/2.26f7cb7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2b341812e042f3fcf9",
+    "url": "/ChuckNorrisJokes/static/js/main.529582c1.chunk.js"
+  },
+  {
+    "revision": "82cf13f72865e6c2fcb8",
+    "url": "/ChuckNorrisJokes/static/js/runtime-main.a3224b87.js"
+  }
+]);
